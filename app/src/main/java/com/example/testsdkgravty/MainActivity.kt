cda218465bef01fa3872413f5fdac22aaeed6343
Loitp <v.loitp@cloudhms.net>
2022-01-19 15:57:37 +0700

    update

--- app/src/main/java/com/example/testsdkgravty/MainActivity.kt
+++ app/src/main/java/com/example/testsdkgravty/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.testsdkgravty
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.gravty.sdk.GravtyRequest
+import com.gravty.sdk.models.MemberResponse
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViews()
+    }
+
+    private fun logD(s: String) {
+        Log.d("loitpp", s)
+    }
+
+    private fun setupViews() {
+        findViewById<Button>(R.id.btLogin).setOnClickListener {
+            logD("Click")
+            GravtyRequest.login(
+                "123", "123",
+                object : GravtyRequest.Callback<MemberResponse?> {
+                    override fun onResponse(response: Response<MemberResponse?>) {
+                        logD("onResponse $response")
+                        if (response.isSuccessful) {
+                            val memberResponse: MemberResponse? = response.body()
+                            logD(">>>memberResponse $memberResponse")
+                        }
+                    }
+
+                    override fun onFailure(t: Throwable) {
+                        logD("onFailure $t")
+                    }
+                }
+            )
+        }
     }
 }
